23b9603614a96873e6af4134aca645f9aabbb594
KenLi8504 <kli22@stuy.edu>
2022-01-06 06:34:50 -0500

    forgot to commit the client side functions

--- socket_networking.c
+++ socket_networking.c
@@ -1,4 +1,4 @@
-#include "pipe_networking.h"
+#include "socket_networking.h"
 
 
 /*=========================
@@ -41,7 +41,7 @@ int server_handshake(int listen_socket) {
   socklen_t sock_size;
   struct sockaddr_storage client_address;
   sock_size = sizeof(client_address);
-  client_socket = accept(sd,(struct sockaddr *)&client_address, &sock_size);
+  client_socket = accept(listen_socket,(struct sockaddr *)&client_address, &sock_size);
   return client_socket;
 }
 
@@ -53,41 +53,19 @@ int server_handshake(int listen_socket) {
   Sets *to_server to the file descriptor for the upstream pipe.
   returns the file descriptor for the downstream pipe.
   =========================*/
-int client_handshake(int *to_server) {
+int client_handshake() {
+  struct addrinfo * hints, * results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM; //TCP socket
+  hints->ai_flags = AI_PASSIVE; //only needed on server
+  getaddrinfo("127.0.0.1", "9845", hints, &results);  //Server sets node to NULL
 
-  int from_server;
-  char buffer[HANDSHAKE_BUFFER_SIZE];
-  char ppname[HANDSHAKE_BUFFER_SIZE];
+  int connect_socket = socket(results->ai_family,results->ai_socktype,results->ai_protocol);
+  connect(connect_socket, results->ai_addr, results->ai_addrlen);
 
-  //make private pipe
-  printf("[client] handshake: making pp\n");
-  sprintf(ppname, "%d", getpid() );
-  mkfifo(ppname, 0600);
-
-  //send pp name to server
-  printf("[client] handshake: connecting to wkp\n");
-  *to_server = open( WKP, O_WRONLY, 0);
-  if ( *to_server == -1 ) {
-    printf("open error %d: %s\n", errno, strerror(errno));
-    exit(1);
-  }
-
-  write(*to_server, ppname, sizeof(buffer));
-  //open and wait for connection
-  from_server = open(ppname, O_RDONLY, 0);
-
-  read(from_server, buffer, sizeof(buffer));
-  /*validate buffer code goes here */
-  printf("[client] handshake: received -%s-\n", buffer);
-
-  //remove pp
-  remove(ppname);
-  printf("[client] handshake: removed pp\n");
-
-  //send ACK to server
-  int r = atoi(buffer) + 1;
-  sprintf(buffer, "%d", r);
-  write(*to_server, buffer, sizeof(buffer));
-
-  return from_server;
+  // Exiting Function
+  free(hints);
+  freeaddrinfo(results);
+  return connect_socket;
 }
